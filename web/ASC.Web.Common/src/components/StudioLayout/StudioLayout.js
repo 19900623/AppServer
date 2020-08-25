09cc2aa3ad4d044b57ffba4340bb1565dfdf74fc
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2020-08-25 15:42:01 +0300

    Remove one of separators

--- web/ASC.Web.Common/src/components/StudioLayout/StudioLayout.js
+++ web/ASC.Web.Common/src/components/StudioLayout/StudioLayout.js
@@ -59,10 +59,9 @@ const getAvailableModules = (modules, currentUser) => {
 
   const isUserAdmin = currentUser.isAdmin;
   const customModules = getCustomModules(isUserAdmin);
-  const separator = getSeparator("nav-products-separator");
   const products = modules.map(m => toModuleWrapper(m));
 
-  return [separator, ...products, ...customModules];
+  return [...products, ...customModules];
 };
 
 const StudioLayoutContainer = withTranslation()(PureStudioLayout);
